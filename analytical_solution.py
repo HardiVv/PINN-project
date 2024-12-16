6d4373b3a8159965ba6a4793eec7d9c796180201
HardiVv <hardiveski@gmail.com>
2024-12-16 15:42:53 +0200

    initial stuffs

--- analytical_solution.py
+++ analytical_solution.py
@@ -1,1 +1,11 @@
+import numpy as np
+import torch
 
+
+def exact_solution(x, t, alpha):
+    """
+    Analytical solution to the 1D heat equation
+    u(x, 0) = sin(pi x),
+    u(0, t) = u(1, t) = 0 (boundary conditions)
+    """
+    return torch.exp(-np.pi**2 * alpha * t) * torch.sin(np.pi * x)
