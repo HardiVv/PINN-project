c93622a00f08f88a3ae196b476ffef37cb9d666d
HardiVv <hardiveski@gmail.com>
2025-01-08 00:07:55 +0200

    added head eq with source

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from models import FCN
 from training import train_pinn
-
+from analytical_solution import heat_eq_1D, heat_eq_1D_with_source
 
 
 def load_params():
@@ -13,30 +13,48 @@ def load_params():
     """
     with open('params.yaml', 'r') as file:
         return yaml.safe_load(file)
-
+    
+def get_exact_solution():
+    params = load_params()
+    solution_type = params['equation']['type']
+    if solution_type == "heat_eq_1D":
+        return heat_eq_1D, False
+    elif solution_type == "heat_eq_1D_with_source":
+        return heat_eq_1D_with_source, True
+    else:
+        raise ValueError("Invalid solution type specified in params.yaml")
 
 def main():
 
     params = load_params()
+    exact_solution_func, with_source = get_exact_solution()
 
     # Set random seed for reproducibility
     seeds = params['training_params']['random_seeds']
     torch.manual_seed(seeds)
     np.random.seed(seeds)
 
-
-    # Initialize PINN model
+# Initialize PINN model
     pinn = FCN(**params['model_params'], inversion=False)
+    
+    # Train PINN with the selected exact solution
+    print("=============")
+    print("Training PINN")
+    print("=============")
+    train_pinn(pinn, params,
+               exact_solution=exact_solution_func,
+               inversion=False,
+               with_source=with_source)
 
-    # Train PINN
-    print("Training PINN...")
-    train_pinn(pinn, params, inversion=False)
-
-    # Perform parameter inversion
+    # Parameter inversion
     pinn_inversion = FCN(**params['model_params'], inversion=True)
-
-    print("Performing parameter inversion...")
-    train_pinn(pinn_inversion, params, inversion=True)
+    print("=============")
+    print("PINN inversion")
+    print("=============")
+    train_pinn(pinn_inversion, params,
+               exact_solution=exact_solution_func,
+               inversion=True,
+               with_source=with_source)
 
 if __name__ == "__main__":
     main()
